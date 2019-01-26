6cdf1c1b11a1aa61d5455c351f53513b2348f089
mibatko <mibatko@gmail.com>
2019-01-26 09:23:09 +0100

    Cleaned up the code

--- src/mmop/client/Controller.java
+++ src/mmop/client/Controller.java
@@ -10,41 +10,28 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
-//Klasa obsługująca GUI
 public class Controller {
-    //TODO: Gracefully disconnecting when closed.
-    //TODO: Handle 'exit' message.
-
-    //Dodatkowe elementy wymagane do nawiązania połączenia - gniazdo sieciowe i strumienie I/O
-    //Prawdopodobnie lepiej byłoby je zawrzeń w innej klasie
     private Socket socket;
     private BufferedReader input;
     private PrintWriter output;
 
     private static final int PORT = 60321;
-    private static final String SERVER_ADDRESS = "18.197.197.247";
+    private static final String SERVER_ADDRESS = "localhost"; //"18.197.197.247";
 
-    //Zmienne poprzedzone @FXML reprezentują poszczególne elementy GUI. Nazwa zmiennej musi pokrywać się z nazwą elementu w pliku FXML
     @FXML
     private TextArea chatTextArea;
 
     @FXML
     private TextField messageTextField;
 
-
-    //Pierwsza metoda kontrolera to przygotowanie wszytskich czynności któe muszą poprzedzić wyświetlenie GUI.
-    //W naszym wypadku nawiązujemy połączenie sieciowe z serwerem.
     @FXML
     public void initialize() {
-        //Zawarte w try-catch gdyż używane klasy i metody generują wyjątki
         try {
             chatTextArea.appendText("Connecting to " + SERVER_ADDRESS + " on port " + PORT + "...\n");
             socket = new Socket(SERVER_ADDRESS, PORT);
             input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             output = new PrintWriter(socket.getOutputStream(), true);
 
-            //Po nawiązaniu połączenia utwórz instancję klasy Netcode, która wielowątkowo obsługuje czynności związane z połączeniem z serwerem
-            //Klasa dziedziczy po klasie Thread dlatego po utworzeniu instancji uruchamiamy nowy wątek metodą start()
             Netcode netcode = new Netcode(this, input);
             netcode.start();
         }
@@ -53,13 +40,11 @@ public class Controller {
         }
     }
 
-    //Metoda suatwiająca pole tekstowe do wpisywania wiadomości jako aktywny element GUI. Aby można było pisać od razu po uruchomieniu aplikacji.
     @FXML
     void focusOnMessageTextField() {
         messageTextField.requestFocus();
     }
 
-    //Kod wywoływany po naciśnieniu przycisku sendMessage
     @FXML
     public void onSendMessageButtonClick() {
         if(!messageTextField.getText().isBlank())
@@ -68,18 +53,15 @@ public class Controller {
         messageTextField.clear();
     }
 
-    //Kod wywolywany po nacisnieniu przycisku clearChat
     @FXML
     public void onClearChatButtonClick() {
         chatTextArea.clear();
     }
 
-    //Wysylanie wiadomosci do serwera przez strumien wyjscia
     private void sendMessageToServer() {
         output.println(messageTextField.getText());
     }
 
-    //Drukowanie tektu w polu chactu
     void printToTextArea(String message) {
         chatTextArea.appendText(message + "\n");
     }
--- src/mmop/client/Netcode.java
+++ src/mmop/client/Netcode.java
@@ -3,24 +3,19 @@ package mmop.client;
 import java.io.BufferedReader;
 import java.io.IOException;
 
-//Klasa obsługująca połączenie z serwerem
 public class Netcode extends Thread {
 
     private BufferedReader input;
     private Controller controller;
 
-    //Konstruktor
     Netcode(Controller controller, BufferedReader input) {
         this.controller = controller;
         this.input = input;
     }
 
-    //Glowne dzialanie klasy obslugujacej połączenie
     @Override
     public void run() {
-        //W nieskonczonej petli sluchaj nadawanych wiadomości przez serwer i drukuj je w polu tektowych chatu.
         try {
-            //TODO: Check if there's a better way of handling infinite loop
             while(true) {
                 String message = input.readLine();
                 controller.printToTextArea(message);
